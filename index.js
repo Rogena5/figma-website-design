7d3f5f745d6de8b9578f02fba97e0efe89119d9a
rogena <rogenaramze10@gmail.com>
2022-11-25 15:08:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navLinks');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active')
+    })
+}
+navSlide()
